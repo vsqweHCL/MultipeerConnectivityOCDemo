6b7be567ffdd461fd79c66feca6774ca79f3c8b4
HCL黄 <hclhuang@hclhuangdemacbook-pro.local>
2016-11-15 22:57:56 +0800

    初始化项目

---
+++ MultipeerConnectivityDemo/MultipeerConnectivityDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MultipeerConnectivityDemo
+//
+//  Created by HCL黄 on 16/11/15.
+//  Copyright © 2016年 HCL黄. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MultipeerConnectivityDemo/MultipeerConnectivityDemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  MultipeerConnectivityDemo
+//
+//  Created by HCL黄 on 16/11/15.
+//  Copyright © 2016年 HCL黄. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
